2206efd046bcc4e80735b59f362c0aa79018a796
BoeJama <111588551+BoeJama@users.noreply.github.com>
2023-02-16 08:58:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-/*
- * declare map as a global variable
- */
 var map;
 
-/*
- * use google maps api built-in mechanism to attach dom events
- */
 google.maps.event.addDomListener(window, "load", function () {
 
-  /*
-   * create map
-   */
   var map = new google.maps.Map(document.getElementById("map_div"), {
     center: new google.maps.LatLng(39.8283, -98.5795),
     zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  /*
-   * create infowindow (which will be used by markers)
-   */
   var infoWindow = new google.maps.InfoWindow();
 
-  /*
-   * marker creater function (acts as a closure for html parameter)
-   */
   function createMarker(options, html) {
     var marker = new google.maps.Marker(options);
     if (html) {
@@ -36,9 +21,6 @@ google.maps.event.addDomListener(window, "load", function () {
     return marker;
   }
 
-  /*
-   * add markers to map
-   */
   var marker0 = createMarker({
     position: new google.maps.LatLng(39.8283, -98.5795),
     map: map,
@@ -90,6 +72,40 @@ function center(){
       position: new google.maps.LatLng(39.8283, -98.5795),
       map: map
     }, "<h1>Marker 2</h1><p>This is marker 2</p>");
+  };
 
+  function liberty(){
+    var marker0 = createMarker({
+      position: new google.maps.LatLng(40.6892, -74.0445),
+      map: map,
+      icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+    }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
+  
+    var marker1 = createMarker({
+      position: new google.maps.LatLng(40.6892, -74.0445),
+      map: map
+    }, "<h1>Marker 1</h1><p>This is marker 1</p>");
+  
+    var marker2 = createMarker({
+      position: new google.maps.LatLng(40.6892, -74.0445),
+      map: map
+    }, "<h1>Marker 2</h1><p>This is marker 2</p>");
+  };
 
+  function hoover(){
+    var marker0 = createMarker({
+      position: new google.maps.LatLng(36.0161, -114.7377),
+      map: map,
+      icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+    }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
+  
+    var marker1 = createMarker({
+      position: new google.maps.LatLng(36.0161, -114.7377),
+      map: map
+    }, "<h1>Marker 1</h1><p>This is marker 1</p>");
+  
+    var marker2 = createMarker({
+      position: new google.maps.LatLng(36.0161, -114.7377),
+      map: map
+    }, "<h1>Marker 2</h1><p>This is marker 2</p>");
   };
